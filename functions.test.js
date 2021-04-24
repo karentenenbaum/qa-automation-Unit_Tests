7d8a9e5ff324d139ba25237a2be0b48f388829fe
karentenenbaum <kjtenenbaum@gmail.com>
2021-04-24 15:48:08 -0300

    created new scripts

--- functions.test.js
+++ functions.test.js
@@ -1,18 +1,33 @@
 import * as functions from "./functions";
 
 //1
-test('texto', () => {
+test('text', () => {
     expect(functions.texto("text")).toBe("text");
 });
 
 //2
-test('suma', () => {
+test('sum', () => {
     expect(functions.suma(4, 7)).toBe(11);
 });
 
 //3
-test('resta', () => {
-    expect(functions.suma(4, 7)).toBe(11);
+test('substraction', () => {
+    expect(functions.resta(88,66)).toBe(22);
 });
 
+//4
+test('multiplication', () => {
+    expect(functions.multiplicacion(7,4)).toBe(28);
+});
+
+//5
+describe('divisions', () => {
+    test('division by zero', () => {
+      expect(functions.division(4,0)).toBe("No se puede dividir por 0");
+    });
+    
+    test('division by a number other than zero', () => {
+      expect(functions.division(4,2)).toBe(2);
+    });
+  });
 
