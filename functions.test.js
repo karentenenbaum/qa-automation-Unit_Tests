a56ddddabb99905aa43ed1cb1252e16f2bbc0f97
karentenenbaum <kjtenenbaum@gmail.com>
2021-04-24 13:01:09 -0300

    modified functions.js & functions.test.js

--- functions.test.js
+++ functions.test.js
@@ -1,11 +1,16 @@
-// const suma = require('./functions');
-
 import * as functions from "./functions";
 
-test('sumar 1 + 2 es igual a 3', () => {
-  expect(functions.suma(1, 2)).toBe(3);
-});
+//1
+test('texto', () => {
+    expect(operations.texto("text")).toBe("text");
+  });
 
-// test('restar 1 - 2 es igual a -1', () => {
-//     expect(functions.resta(1, 2)).toBe(3);
-//   });
+  //2
+  test('suma', () => {
+    expect(operations.suma(4,7)).toBe(11);
+  });
+
+  //3
+  
+
+
