dbdd3291d442448eb5c517fdd459751868021632
karentenenbaum <kjtenenbaum@gmail.com>
2021-04-24 15:13:31 -0300

    created scripts

--- functions.test.js
+++ functions.test.js
@@ -2,15 +2,17 @@ import * as functions from "./functions";
 
 //1
 test('texto', () => {
-    expect(operations.texto("text")).toBe("text");
-  });
+    expect(functions.texto("text")).toBe("text");
+});
 
-  //2
-  test('suma', () => {
-    expect(operations.suma(4,7)).toBe(11);
-  });
+//2
+test('suma', () => {
+    expect(functions.suma(4, 7)).toBe(11);
+});
 
-  //3
-  
+//3
+test('resta', () => {
+    expect(functions.suma(4, 7)).toBe(11);
+});
 
 
